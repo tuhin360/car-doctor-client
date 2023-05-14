3589ed1d056c72babe70283e2422808197a0906a
tuhin <tuhin153695@gmail.com>
2023-05-14 16:18:43 +0600

    banner added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
-import Home from "../pages/Home/Home";
+import Home from "../pages/Home/Home/Home";
+ 
 
 const router = createBrowserRouter([
     {
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <h2>This is home</h2>
         </div>
     );
