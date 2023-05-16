49adc822f581a4e472b97d0dcd2068320205eb10
tuhin <tuhin153695@gmail.com>
2023-05-16 15:45:25 +0600

    firebase signup and login authentication added

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -17,7 +17,8 @@ const ServiceCard = ({service}) => {
         <p className="text-xl text-orange-600 font-bold">Price: ${price}</p>
         <div className="card-actions flex justify-between ">
           <button className="btn btn-primary">Buy Now</button>
-          <button className='items-center text-orange-600 font-bold text-2xl border-2 border-sky-500 rounded-full p-2 mt-2'><FaArrowRight /></button>
+          <button className='items-center text-orange-600 font-bold text-2xl border-2 border-gray
+          -500 rounded-full p-2 mt-2'><FaArrowRight /></button>
         </div>
       </div>
     </div>
@@ -25,5 +26,9 @@ const ServiceCard = ({service}) => {
 };
 
 export default ServiceCard;
+
+
+
+
 
 
---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,56 @@
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import app from "../firebase.config";
+
+
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // create user
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // signIn with email and password 
+    const  signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+
+    // user asa nki ni ta check korar jonno
+    useEffect( () => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) =>{
+            setUser(currentUser);
+            console.log('Current user: ',currentUser);
+            setLoading(false);
+        })
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
