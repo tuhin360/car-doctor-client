31f6b2d026e42ff8b998605706c7b0a4ea26599e
tuhin <tuhin153695@gmail.com>
2023-05-18 00:31:28 +0600

    booking service client added

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,9 +1,14 @@
 import { FaArrowRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 
+ 
+// eslint-disable-next-line react/prop-types
 const ServiceCard = ({service}) => {
 
-    const {title, img, price} = service;
+ 
+    // eslint-disable-next-line react/prop-types
+    const {_id, title, img, price} = service;
 
   return (
     <div className="card card-compact w-96 bg-base-100 shadow-xl">
@@ -16,7 +21,9 @@ const ServiceCard = ({service}) => {
         <h2 className="card-title text-2xl font-bold">{title}</h2>
         <p className="text-xl text-orange-600 font-bold">Price: ${price}</p>
         <div className="card-actions flex justify-between ">
-          <button className="btn btn-primary">Buy Now</button>
+          <Link to={`/book/${_id}`}>
+             <button className="btn btn-primary">Book Now</button>
+          </Link>
           <button className='items-center text-orange-600 font-bold text-2xl border-2 border-gray
           -500 rounded-full p-2 mt-2'><FaArrowRight /></button>
         </div>
