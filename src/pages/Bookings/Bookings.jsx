21cbc6cfdb1b43b8c72a4006a51da8543ff56524
tuhin <tuhin153695@gmail.com>
2023-05-19 03:33:15 +0600

    crud for client added

---
+++ src/pages/Bookings/Bookings.jsx
@@ -0,0 +1,96 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
+import BookingRow from "./BookingRow";
+
+const Bookings = () => {
+  const { user } = useContext(AuthContext);
+  const [bookings, setBookings] = useState([]);
+
+  const url = `http://localhost:5000/bookings?email=${user?.email}`;
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setBookings(data));
+  }, [url]);
+
+  // delete bookings service
+  const handleDelete = (id) => {
+    const proceed = confirm("Are you sure you want to delete");
+    if (proceed) {
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("Deleted successfully");
+            const remaining = bookings.filter((booking) => booking._id !== id);
+            setBookings(remaining);
+          }
+        });
+    }
+  };
+
+  // confirm booking service
+  const handleBookingConfirm = (id) => {
+    fetch(`http://localhost:5000/bookings/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({status: 'confirm'})
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          // update state
+          const remaining = bookings.filter(booking => booking._id !== id);
+          const updated = bookings.find(booking => booking._id === id);
+          updated.status = 'confirm';
+          const newBookings = [updated, ...remaining];
+          setBookings(newBookings);
+
+        }
+      });
+  };
+
+  return (
+    <div>
+      <h2 className="text-5xl font-bold text-center mb-4">
+        Your Bookings: {bookings.length}
+      </h2>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Delete</th>
+              <th>Image</th>
+              <th>Service</th>
+              <th>Date</th>
+              <th>Price</th>
+              <th>Customer</th>
+              <th>Email</th>
+              <th>Confirm</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking) => (
+              <BookingRow
+                key={booking._id}
+                booking={booking}
+                handleDelete={handleDelete}
+                handleBookingConfirm={handleBookingConfirm}
+              ></BookingRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Bookings;
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -21,9 +21,14 @@ const Navbar = () => {
         <Link to="/about">About</Link>
       </li>
       {user?.email ? (
-        <li>
-          <button onClick={handleLogOut}>Logout</button>
-        </li>
+        <>
+          <li>
+            <Link to="/bookings">My Bookings</Link>
+          </li>
+          <li>
+            <button onClick={handleLogOut}>Logout</button>
+          </li>
+        </>
       ) : (
         <li>
           <Link to="/login">Login</Link>
